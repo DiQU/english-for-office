faa2d941ddacdc30125b200324344b203c5a2222
DiQU <dieqju@gmail.com>
2019-03-29 13:12:54 +0800

    new function handleConversation, renderTitle,renderAudio,renderConversation

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fsPromises = require('fs').promises
 let sections = []
 let title = []
 let output = []
+let conversation = []
 
 fsPromises
     .readFile('Input.txt', 'utf8')
@@ -10,9 +11,17 @@ fsPromises
         console.log('讀入 input.txt 成功:')
         sections = sectionlize(data.trim(), sectionsKeyword)
         title = handleTitle(sections[0])
+        renderTitle()
+        renderAudio()
+        conversation = handleConversation(sections[1])
+        renderConversation()
+
         let wFilename = 'unit' + title[1]
-        output.push(wFilename + ':' + title[2])
-        return fsPromises.writeFile('./units/' + wFilename + '.md', sections, 'utf8')
+        return fsPromises.writeFile(
+            './units/' + wFilename + '.md',
+            output.join(''),
+            'utf8'
+        )
     })
     .then(() => {
         console.log('寫入 uint.md 成功')
@@ -57,6 +66,35 @@ function sectionlize(data, sectionsKeyword) {
 function handleTitle(data) {
     // 1.(第幾單元) 2.(標題名稱)
     let regexp = /- ?(\d+) -(?:\r\n|\n)(.+)/
-    const title = regexp.exec(data)
+    const title = data.match(regexp)
     return title
+}
+
+// 處理 CONVERSATION 區域
+function handleConversation(data) {
+    let regexp = /(?:M|W):\s.*/g
+    const result = data.match(regexp)
+    return result
+}
+
+function renderTitle() {
+    let data = `Uint${title[1]}:${title[2]}`
+    let markdown = `# ${data}\n\n`
+    output.push(markdown)
+}
+
+function renderAudio() {
+    let url = '#'
+    let data = `<audio controls preload="none"><source src="${url}"></audio>`
+    let markdown = `${data}\n\n`
+    output.push(markdown)
+}
+
+function renderConversation() {
+    let data = []
+    for (let i = 0; i < conversation.length; i++) {
+        data[i] = `* ${conversation[i]}`
+    }
+    let markdown =`## ${sectionsKeyword[1]}\n${data.join('\n')}\n`
+    output.push(markdown)
 }
